bf97ebb0914f0562d015c72c5ef8deb9ba03c6cc
chamila <chamila.sujeewa@gmail.com>
2024-09-06 13:05:07 +1200

    Add ability to inherit debugger providers from global config
    This change adds a setup config option to inherit global debugger
    adapter configuration instead of having to redefine it in the project
    specific config file. Since some adapters need the absolute path to the
    binary having it in the project local file might not be portable.
    
    This change adds the inherit_adapters setup option, by default set to
    false. Setting it to false will keep the current behavior, which is to
    totally ignore the global nvim-dap configuration. Setting it to true,
    which should be explicitly done,  will not read the project local
    adapters section, instead using the global section.
    
    This change will not break existing config since setup() function is
    optional and defaults work to override adapters.
    
    todo: merge global and local adapter sections instead of replacing.

--- lua/nvim-dap-projects.lua
+++ lua/nvim-dap-projects.lua
@@ -1,6 +1,16 @@
 local M = {}
 
-M.config_paths = {"./.nvim-dap/nvim-dap.lua", "./.nvim-dap.lua", "./.nvim/nvim-dap.lua"}
+local defaults = {
+    inherit_adapters = false, -- inherit adapter configuration from global
+}
+
+M.config_paths = { "./.nvim-dap/nvim-dap.lua", "./.nvim-dap.lua", "./.nvim/nvim-dap.lua" }
+
+M.options = {}
+
+function M.setup(user_options)
+    M.options = vim.tbl_extend("force", defaults, user_options or {})
+end
 
 function M.search_project_config()
     if not pcall(require, "dap") then
@@ -20,7 +30,12 @@ function M.search_project_config()
         return
     end
     vim.notify("[nvim-dap-projects] Found nvim-dap configuration at." .. project_config, vim.log.levels.INFO, nil)
-    require('dap').adapters = (function() return {} end)()
+
+    -- default is false to not break existing configuration
+    if not M.options.inherit_adapters then
+        require('dap').adapters = (function() return {} end)()
+    end
+
     require('dap').configurations = (function() return {} end)()
     vim.cmd(":luafile " .. project_config)
 end
